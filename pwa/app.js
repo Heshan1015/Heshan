35ba67cf84e38c40bd2b10fc0c5552cc31b6d3bd
Heshan <cb015742@students.apiit.lk>
2025-08-28 08:59:45 +0530

    first commit

---
+++ pwa/app.js
@@ -0,0 +1,11 @@
+document.getElementById('ping').addEventListener("click", function(){
+
+    document.getElementById('out').textContent="JS is working";
+
+})
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('sw.js')
+    .then(()=> console.log('Service Worker registered'))
+    .catch(err => console.log('SW registration failed', err));
+
+}
